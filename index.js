be8cdb77b2014059b7277dc2189719bc78bfd878
hsfzxjy <hsfzxjy@gmail.com>
2021-05-08 18:56:34 +0800

    Update

---
+++ index.js
@@ -0,0 +1,15 @@
+hexo.config.imbedrender = Object.assign({
+    enable: false,
+    bin: '',
+    prefix: '@@',
+}, hexo.config.imbedrender)
+
+hexo.extend.filter.register('template_locals', require('./lib/filter/template_locals/lodash'))
+
+require('./lib/filter/before_post_render/hackHighlight')
+// hexo.extend.filter.register('before_post_render', require('./lib/filter/before_post_render/hackHighlight'), 11)
+hexo.extend.filter.register('before_post_render', require('./lib/filter/before_post_render/mathjax'), 11)
+hexo.extend.filter.register('before_post_render', require('./lib/filter/before_post_render/imbedRender'), 9)
+
+hexo.extend.filter.register('after_post_render', require('./lib/filter/after_post_render/typography'), 11)
+hexo.extend.filter.register('after_post_render', require('./lib/filter/after_post_render/hl_label'), 11)
