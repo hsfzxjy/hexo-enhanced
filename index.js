fd0358fae3f6bcba95ca9bb05e49af153a39d5fd
hsfzxjy <hsfzxjy@gmail.com>
2022-01-03 20:41:45 +0800

    Delete garbage lines

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 hexo.config.imbedrender = Object.assign(
-    {
-        enable: false,
-        bin: "",
-        prefix: "@@",
-    },
-    hexo.config.imbedrender
+  {
+    enable: false,
+    bin: "",
+    prefix: "@@",
+  },
+  hexo.config.imbedrender
 )
 
-hexo.extend.filter.register("template_locals", require("./lib/filter/template_locals/lodash"))
+hexo.extend.filter.register(
+  "template_locals",
+  require("./lib/filter/template_locals/lodash")
+)
 
 require("./lib/filter/before_post_render/hackHighlight")
-// hexo.extend.filter.register('before_post_render', require('./lib/filter/before_post_render/hackHighlight'), 11)
-hexo.extend.filter.register("before_post_render", require("./lib/filter/before_post_render/mathjax"), 11)
-hexo.extend.filter.register("before_post_render", require("./lib/filter/before_post_render/graphviz"), 9)
-hexo.extend.filter.register("before_post_render", require("./lib/filter/before_post_render/imbedRender"), 9)
+hexo.extend.filter.register(
+  "before_post_render",
+  require("./lib/filter/before_post_render/mathjax"),
+  11
+)
+hexo.extend.filter.register(
+  "before_post_render",
+  require("./lib/filter/before_post_render/graphviz"),
+  9
+)
 
-hexo.extend.filter.register("after_post_render", require("./lib/filter/after_post_render/typography"), 11)
-hexo.extend.filter.register("after_post_render", require("./lib/filter/after_post_render/hl_label"), 11)
-hexo.extend.filter.register("after_post_render", require("./lib/filter/after_post_render/inline_notes"), 11)
+hexo.extend.filter.register(
+  "after_post_render",
+  require("./lib/filter/after_post_render/typography"),
+  11
+)
+hexo.extend.filter.register(
+  "after_post_render",
+  require("./lib/filter/after_post_render/hl_label"),
+  11
+)
+hexo.extend.filter.register(
+  "after_post_render",
+  require("./lib/filter/after_post_render/inline_notes"),
+  11
+)
